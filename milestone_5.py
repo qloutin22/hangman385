eca4f5137a26eb780b90cdfe236714e62f855580
qloutin22 <114169147+qloutin22@users.noreply.github.com>
2023-11-30 16:11:14 +0000

    Update milestone_5.py

--- milestone_5.py
+++ milestone_5.py
@@ -1,5 +1,5 @@
 import random 
-word_list = ["Mango" ,"Apple","Pomgrante","Orange"," Banana"]
+word_list = ["Mango" ,"Apple","Pomgrante","Orange","Banana"]
 
 class Hangman :
     def __init__ (self, word_list, num_lives=5):
@@ -20,11 +20,16 @@ class Hangman :
                 for index, letter in enumerate(self.word):
                     if letter.lower() == guess:
                         self.word_guessed[index] = letter
-                self.num_letters =-1
+                self.num_letters = self.num_letters-1
+                print(self.word_guessed)
             else:
                 print("Sorry," ,guess," is not in the word. Try again.")
-                self.num_lives =-1
+                self.num_lives = self.num_lives -1
                 print("You have", self.num_lives ,"lives left")
+                if self.num_lives == 0:
+                    print("You Lost!")
+
+                
 
     
     
@@ -35,30 +40,33 @@ class Hangman :
 
             if len(guess) != 1 or guess.isalpha() != True:
                 print("Invalid letter. Please, enter a single alphabetical character.") 
-                Hangman.ask_for_input()
+                Hangman.ask_for_input(self)
             elif guess in self.list_of_guesses:
                 print("You already tried that letter!") 
-                Hangman.ask_for_input()
+                Hangman.ask_for_input(self)
             else:
                 self.check_guess(guess) 
                 self.list_of_guesses.append(guess)
                 print(self.list_of_guesses)
-                Hangman.ask_for_input()
+                Hangman.ask_for_input(self)
             return 
         
 
 def play_game (word_list):
     num_lives = 5
     game = Hangman(word_list,num_lives)
-
+    
     while True:
-        if game.num_lives == 0 :
+        if num_lives == 0 :
             print("You LOST!")
+            print(num_lives)
             break
         if game.num_letters > 0:
             game.ask_for_input()
-        if game.num_lives and game.num_letters !=0:
+            print(num_lives)
+        if num_lives and game.num_letters !=0 :
             print("Congradulations you have won!!!")
+            print(num_lives)
             break
 
 
