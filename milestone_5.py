2e033574870c5028c8dda830ad0e48d05b8ca2ba
quanna <quannaloutin@gmail.com>
2023-12-01 16:15:43 +0000

    Update

--- milestone_5.py
+++ milestone_5.py
@@ -15,57 +15,52 @@ class Hangman :
     def check_guess(self, guess):
         guess = guess.lower()
         if len(guess) and guess.isalpha() == True: 
-            if any(guess.lower() in word.lower() for word in word_list):
+            if any(guess.lower() in word.lower() for word in self.word):
                 print("Good guess!", guess , "is in the word.")
                 for index, letter in enumerate(self.word):
                     if letter.lower() == guess:
                         self.word_guessed[index] = letter
                 self.num_letters = self.num_letters-1
                 print(self.word_guessed)
-                if guess in self.word == self.word_guessed:
-                    print ("You won!")
             else:
                 print("Sorry," ,guess," is not in the word. Try again.")
                 self.num_lives = self.num_lives -1
                 print("You have", self.num_lives ,"lives left")
-                if self.num_lives == 0:
-                    print("You Lost!")
+        
             
     def ask_for_input(self):
         
         while True: 
             guess = str(input("Please input a letter?"))
-
             if len(guess) != 1 or guess.isalpha() != True:
                 print("Invalid letter. Please, enter a single alphabetical character.") 
-                Hangman.ask_for_input(self)
+                break
             elif guess in self.list_of_guesses:
                 print("You already tried that letter!") 
-                Hangman.ask_for_input(self)
+                break
             else:
                 self.check_guess(guess) 
                 self.list_of_guesses.append(guess)
                 print(self.list_of_guesses)
-                Hangman.ask_for_input(self)
-            return 
+                break
+            
         
 
 def play_game (word_list):
     num_lives = 5
     game = Hangman(word_list,num_lives)
-    
     while True:
-        if num_lives == 0 :
+        if game.num_lives == 0 :
             print("You LOST!")
-            print(num_lives)
             break
-        if game.num_letters > 0:
+        elif game.num_letters > 0:
             game.ask_for_input()
-            print(num_lives)
-            if num_lives and game.num_letters !=0  :
-                print("Congradulations you have won!!!")
-                print(num_lives)
-                break
+        else:
+            game.num_lives and game.num_letters !=0 
+            print("Congradulations you have won!!!")
+            break
+        
+
 
 
 play_game(word_list)
